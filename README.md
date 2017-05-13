6fec6b7b1d03dec10d2992ce9dd34338341e7b84
stijn <stijn@solydxk>
2017-05-13 19:35:53 +0200

    added histogram

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # windspeed_arduino
+read out wind speed sensor Eltako WS with an arduino due
--- WindSpeedSensing/WindSpeedSensing.ino
+++ WindSpeedSensing/WindSpeedSensing.ino
@@ -8,6 +8,10 @@
 
 */
 
+// histogram library downloaded from arduino playground
+#include <histogram.h>
+#include <DueFlashStorage.h>
+
 const byte pin_led = 13;
 const byte pin_interrupt = 32;
 
@@ -22,7 +26,15 @@ int index_buffer = 0;
 long time_delta[3] = {0, 0, 0};
 long temporary;
 float sensor_rps;
-float v_wind_kph
+float v_wind_kph;
+
+int time_delay = 1000;
+
+double buckets[] = {0, 2, 4, 6, 8, 10, 12, 14, 15, 16, 18, 20, 25, 30, 35, 40, 45, 50, 60};
+Histogram hist(19, buckets);
+
+DueFlashStorage dueFlashStorage;
+
 
 void setup() {
   //start serial connection
@@ -35,9 +47,6 @@ void setup() {
 }
 
 void loop() {
-  //read the pushbutton value into a variable
-  int sensorVal = digitalRead(pin_interrupt);
-  //print out the value of the pushbutton
 
   // copy buffers
   timestamps_index = timestamps_index_cp;
@@ -46,9 +55,9 @@ void loop() {
   }
 
 
-  for (byte i = 0; i < 8; i = i + 1) {
-    Serial.println(timestamps[i]);
-  }
+  //  for (byte i = 0; i < 8; i = i + 1) {
+  //    Serial.println(timestamps[i]);
+  //  }
 
   Serial.println("--");
   // process data from circular buffer
@@ -60,17 +69,17 @@ void loop() {
       index_buffer = index_buffer + 8;
     }
     time_delta[i] = timestamps[index_buffer];
-//    Serial.println(timestamps[index_buffer]);
+    //    Serial.println(timestamps[index_buffer]);
 
     index_buffer = timestamps_index - 2 * (i + 1);
     if (index_buffer < 0) {
       index_buffer = index_buffer + 8;
     }
     time_delta[i] = time_delta[i] - timestamps[index_buffer];
-//    Serial.println(timestamps[index_buffer]);
+    //    Serial.println(timestamps[index_buffer]);
   }
 
-
+  // sort time_delta buffer
   for (byte j = 0; j < 3; j++)
   {
     for (byte i = 1; i < 3 - j; i++)
@@ -84,25 +93,34 @@ void loop() {
     }
   }
 
-//    for (byte i = 0; i < 3; i = i + 1) {
-//      Serial.println(time_delta[i]);
-//    }
+  //    for (byte i = 0; i < 3; i = i + 1) {
+  //      Serial.println(time_delta[i]);
+  //    }
 
 
-  // take second element out of buffer
+  // take second element out of buffer (median)
   sensor_rps = 1000000 / (float)time_delta[1];
   v_wind_kph = 1.761 / (1 + sensor_rps) + 3.013 * sensor_rps;
 
-  Serial.println(sensor_rps);
+  Serial.println("wind speed:");
+  Serial.println(v_wind_kph);
+
+  // add windpseed to histogram
+  hist.add(v_wind_kph);
+
+  Serial.print(hist.count());
+  for (int i = 0; i < hist.size(); i++)
+  {
+    Serial.print("\t");
+    // gives percentage per bucket
+    // Serial.print(hist.bucket(i));
+    Serial.print(hist.frequency(i), 2);
+  }
+
+
   Serial.println("----");
 
-  //  if (sensorVal == HIGH) {
-  //    digitalWrite(pin_led, LOW);
-  //  } else {
-  //    digitalWrite(pin_led, HIGH);
-  //  }
-
-  delay(100);
+  delay(time_delay);
 }
 
 void get_timestamp() {
